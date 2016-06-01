083f00832945221bfec8647eb16e5e84657fb0f5
divol <jacques.divol@laposte.net>
2016-06-01 16:36:53 +0200

    ajout du Bridging-Header.h

---
+++ ChronoTir-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  ChronoTir-Bridging-Header.h
+//  ChronoTir
+//
+//  Created by divol on 01/06/2016.
+//  Copyright © 2016 Nicolas CHEVALIER. All rights reserved.
+//
+
+#import "VPRangeSlider/VPRangeSlider.h"
